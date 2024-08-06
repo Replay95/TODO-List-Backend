6f40e46bdf4f6991ff65e39b55da094e13b99037
Replay95 <lia0528Ma@gmail.com>
2024-08-06 11:57:07 +0900

    エラーメッセージなどの修正

--- index.js
+++ index.js
@@ -26,16 +26,16 @@ app.post("/api/login", (req, res) => {
     (errors, results) => {
       if (errors) throw errors;
       if (results.rowCount === 0 || null) {
-        return res.status(404).send("ユーザーネームが違います。");
+        return res.status(404).send("ユーザーネームが違います");
       }
       const user = results.rows[0];
       if (user.email !== email) {
-        return res.status(401).send("メールアドレスが違います。");
+        return res.status(401).send("メールアドレスが違います");
       }
       if (user.password !== password) {
-        return res.status(401).send("パスワードが違います。");
+        return res.status(401).send("パスワードが違います");
       }
-      return res.status(200).send("ログインに成功しました。");
+      return res.status(200).send("ログインに成功しました");
     }
   );
 });
@@ -50,7 +50,7 @@ app.post("/api/signup", async (req, res) => {
     );
 
     if (checkUsername.rows.length > 0) {
-      return res.status(409).send("このユーザーネームは使用されています。");
+      return res.status(409).send("このユーザーネームは使用されています");
     }
 
     const checkEmail = await pool.query(
@@ -59,17 +59,17 @@ app.post("/api/signup", async (req, res) => {
     );
 
     if (checkEmail.rows.length > 0) {
-      return res.status(409).send("このメールアドレスは使用されています。");
+      return res.status(409).send("このメールアドレスは使用されています");
     }
-    const result = await pool.query(
+    await pool.query(
       "INSERT INTO users (username, email, password) VALUES ($1, $2, $3)",
       [username, email, password]
     );
 
-    res.status(201).send("ユーザーの作成に成功しました。");
+    res.status(201).send("ユーザーの作成に成功しました");
   } catch (err) {
     console.error("Error executing query:", err);
-    res.status(500).send("ユーザーの作成に失敗しました。");
+    res.status(500).send("サーバー側でエラーが発生しました");
   }
 });
 
